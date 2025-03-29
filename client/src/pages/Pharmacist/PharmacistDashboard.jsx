7014a4cdf4c3d51be9acb23403a58b166f2f6298
XPRISTO <aminhamrouni82@gmail.com>
2025-03-29 17:29:36 +0100

    begin CRUD pharmacist

--- client/src/pages/Pharmacist/PharmacistDashboard.jsx
+++ client/src/pages/Pharmacist/PharmacistDashboard.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import "./PharmacistDashboard.css";
+import PharmaNavbar from "./PharmaNavbar/PharmaNavbar";
+import PharmacistSidebar from "./PharmacistSidebar/PharmacistSidebar";
 const PharmacistDashboard = () => {
   return (
-    <div>PharmacistDashboard</div>
-  )
-}
+    <div className="dashboard-all">
+      <div className="pharmacist-dashboard">
+        <div className="pharmacist-navbar">
+          <PharmaNavbar />
+        </div>
+        <div className="pharma-sidebar"> 
+          <PharmacistSidebar/>
+        </div>
+        <div className="pharma-cards">3</div>
+        <div className="pharma-content">4</div>
+      </div>
+    </div>
+  );
+};
 
-export default PharmacistDashboard
+export default PharmacistDashboard;
---
+++ client/src/pages/Pharmacist/PharmacistSidebar/PharmacistSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  './PharmacistSidebar.css'
+const PharmacistSidebar = () => {
+  return (
+    <div className='pharmacist__sidebar'>
+        
+    </div>
+  )
+}
+
+export default PharmacistSidebar
